4701336e520135ae229bf286102dd053149032c0
kolya8077 <kolya8077@gmail.com>
2022-09-15 23:09:04 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -10,11 +10,12 @@ import {
   SearchFormInput,
 } from 'components/Searchbar/searchbar.style.jsx';
 
-function Searchbars({onSubmit}) {
+function Searchbars({ onSubmit }) {
   const [query, setQuery] = useState('');
 
   const handleChange = e => {
-    setQuery(e.currentTarget.value)
+    if (e.currentTarget.value === query) return
+    setQuery(e.currentTarget.value);
   };
 
   const handleSubmit = e => {
@@ -24,37 +25,32 @@ function Searchbars({onSubmit}) {
       return;
     }
     onSubmit(query);
-    reset();
   };
 
-  const reset = () => {
-    setQuery('')
-  };
+  return (
+    <Searchbar>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchFormButton type="submit">
+          <MdSearch size={30} />
+          <SearchFormButtonLabel>Search</SearchFormButtonLabel>
+        </SearchFormButton>
 
-    return (
-      <Searchbar>
-        <SearchForm onSubmit={handleSubmit}>
-          <SearchFormButton type="submit">
-            <MdSearch size={30} />
-            <SearchFormButtonLabel>Search</SearchFormButtonLabel>
-          </SearchFormButton>
+        <SearchFormInput
+          type="text"
+          name="query"
+          autocomplete="off"
+          autoFocus={true}
+          placeholder="Search images and photos"
+          value={query}
+          onChange={handleChange}
+        />
+      </SearchForm>
+    </Searchbar>
+  );
+}
 
-          <SearchFormInput
-            type="text"
-            name="query"
-            autocomplete="off"
-            autoFocus={true}
-            placeholder="Search images and photos"
-            value={query}
-            onChange={handleChange}
-          />
-        </SearchForm>
-      </Searchbar>
-    );
-  }
-
-  Searchbars.propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
+Searchbars.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default Searchbars;
